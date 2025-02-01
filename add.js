b8bad5d9dfd072a425980a22b2d97773d77009e0
Jasmine <52604018+hiimjustin000@users.noreply.github.com>
2025-02-01 15:25:40 -0500

    Promote a few people, add newlines

--- add.js
+++ add.js
@@ -1,4 +1,11 @@
-require("fs").writeFileSync(require("path").join(__dirname, "v2", process.argv[2] + ".json"), JSON.stringify({
+const fs = require("fs");
+const path = require("path");
+fs.writeFileSync(path.join(__dirname, "badges", process.argv[2] + ".json"), JSON.stringify({
     name: process.argv[3],
     badge: parseInt(process.argv[4])
-}, null, 2));
+}, null, 2) + "\n");
+fs.writeFileSync(path.join(__dirname, "badges.json"), JSON.stringify(require("./badges.json").map(x => x.id == process.argv[2] ? {
+    id: x.id,
+    name: process.argv[3],
+    badge: parseInt(process.argv[4])
+} : x), null, 2) + "\n");
