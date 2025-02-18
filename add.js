44ef456f9786143bcb6160dbaba3b4b35778f34d
Jasmine <52604018+hiimjustin000@users.noreply.github.com>
2025-02-18 00:43:54 -0500

    Update things

--- add.js
+++ add.js
@@ -4,7 +4,15 @@ fs.writeFileSync(path.join(__dirname, "badges", process.argv[2] + ".json"), JSON
     name: process.argv[3],
     badge: parseInt(process.argv[4])
 }, null, 2) + "\n");
-fs.writeFileSync(path.join(__dirname, "badges.json"), JSON.stringify(require("./badges.json").map(x => x.id == process.argv[2] ? {
+const badges = require("./badges.json");
+if (!badges.find(x => x.id == process.argv[2])) {
+    badges.push({
+        id: parseInt(process.argv[2]),
+        name: process.argv[3],
+        badge: parseInt(process.argv[4])
+    });
+}
+fs.writeFileSync(path.join(__dirname, "badges.json"), JSON.stringify(badges.map(x => x.id == process.argv[2] ? {
     id: x.id,
     name: process.argv[3],
     badge: parseInt(process.argv[4])
