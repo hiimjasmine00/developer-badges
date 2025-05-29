53a43f7e50c5909df124a7a7a0401cf0fa10f785
Jasmine <52604018+hiimjasmine00@users.noreply.github.com>
2025-05-29 08:08:48 -0400

    slideglide

--- add.js
+++ add.js
@@ -1,19 +1,24 @@
 const fs = require("fs");
 const path = require("path");
-fs.writeFileSync(path.join(__dirname, "badges", process.argv[2] + ".json"), JSON.stringify({
-    name: process.argv[3],
-    badge: parseInt(process.argv[4])
-}, null, 2) + "\n");
+const isZero = Number.isNaN(parseInt(process.argv[2])) || parseInt(process.argv[2]) <= 0;
+if (!isZero) {
+    fs.writeFileSync(path.join(__dirname, "badges", process.argv[2] + ".json"), JSON.stringify({
+        name: process.argv[3],
+        badge: parseInt(process.argv[4])
+    }, null, 2) + "\n");
+}
 const badges = require("./badges.json");
-if (!badges.find(x => x.id == process.argv[2])) {
+if ((isZero && !badges.find(x => x.name == process.argv[3])) || (!isZero && !badges.find(x => x.id == process.argv[2]))) {
     badges.push({
         id: parseInt(process.argv[2]),
         name: process.argv[3],
         badge: parseInt(process.argv[4])
     });
 }
-fs.writeFileSync(path.join(__dirname, "badges.json"), JSON.stringify(badges.map(x => x.id == process.argv[2] ? {
-    id: x.id,
-    name: process.argv[3],
-    badge: parseInt(process.argv[4])
-} : x), null, 2) + "\n");
+fs.writeFileSync(path.join(__dirname, "badges.json"), JSON.stringify(badges.map(x => {
+    return (isZero && x.name == process.argv[3]) || (!isZero && x.id == process.argv[2]) ? {
+        id: x.id,
+        name: process.argv[3],
+        badge: parseInt(process.argv[4])
+    } : x;
+}), null, 2) + "\n");
